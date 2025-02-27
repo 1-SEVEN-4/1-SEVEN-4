b59b69774023db4122cbdd9e4f87d40835e79c1f
ohhyeon2 <dhgus1235@gmail.com>
2025-02-27 18:17:29 +0900

    style: eslint 컨벤션에 따라서 변경

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-import express from "express";
-import cors from "cors";
+import express from 'express'
+import cors from 'cors'
 
-import { PORT } from "./config/index.js";
+import { PORT } from './config/index.js'
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-app.listen(PORT || 3000, () => console.log(`server on ${PORT}`));
+app.use('', '')
+
+app.listen(PORT || 3000, () => console.log(`server on ${PORT}`))
--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,12 @@
-import dotenv from "dotenv";
-dotenv.config({ debug: true, path: ".env" });
+import dotenv from 'dotenv'
+dotenv.config({ debug: true, path: '.env' })
 
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT
 
 export const POSTGRES_CONFIG = {
-  user: "사용자 이름",
-  host: "로컬호스트",
-  database: "DB명",
-  password: "사용자 비밀번호",
+  user: '사용자 이름',
+  host: '로컬호스트',
+  database: 'DB명',
+  password: '사용자 비밀번호',
   port: 5432,
-};
+}
