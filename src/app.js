1253bdee4c3cc3ca36aa0c7609e9f301d10259e8
ohhyeon2 <dhgus1235@gmail.com>
2025-02-27 17:45:29 +0900

    feat: cors, express.json 추가

--- src/app.js
+++ src/app.js
@@ -4,5 +4,7 @@ import cors from "cors";
 import { PORT } from "./config/index.js";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT || 3000, () => console.log(`server on ${PORT}`));
