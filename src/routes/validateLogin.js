00be4a41353d71b70e5de1b674fb214b794f632c
MINSOO SHIN <minso0317@naver.com>
2025-03-04 15:28:36 +0900

    feat: validate Login 닉네임, 비밀번호 생성시 유효성 설정 닉네임, 비밀번호 일치여부 검증 설정

---
+++ src/routes/validateLogin.js
@@ -0,0 +1,66 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export function validateLoginCondition() {
+  return async (req, res, next) => {
+    try {
+      const regexNickname = /^[A-Za-z0-9]{4,12}$/;
+      const regexPassword =
+        /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()])[a-zA-Z\d!@#$%^&*()]{4,16}$/;
+
+      const { memberNickname, memberPassword } = await req.body;
+
+      if (!regexNickname.test(memberNickname)) {
+        return res.status(400).json({ message: '닉네임이 유효하지 않습니다.' });
+      }
+
+      if (!regexPassword.test(memberPassword)) {
+        return res
+          .status(400)
+          .json({ message: '비밀번호가 유효하지 않습니다.' });
+      }
+      return next();
+    } catch (e) {
+      console.log('Error occured', e);
+      return res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+    } finally {
+      console.log('Finished');
+    }
+  };
+}
+
+export function validateLoginInfo() {
+  return async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { memberNickname, memberPassword } = req.body;
+
+      const member = await prisma.member.findUnique({
+        where: { id },
+      });
+
+      if (!member) {
+        return res
+          .status(404)
+          .json({ message: '회원 정보를 찾을 수 없습니다.' });
+      }
+
+      if (member.memberNickname !== memberNickname) {
+        return res.status(403).json({ message: '닉네임이 일치하지 않습니다.' });
+      }
+
+      if (member.memberPassword !== memberPassword) {
+        return res
+          .status(403)
+          .json({ message: '비밀번호가 일치하지 않습니다.' });
+      }
+      return next();
+    } catch (error) {
+      console.error('Error deleting member:', error);
+      return res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+    } finally {
+      console.log('Finished');
+    }
+  };
+}
