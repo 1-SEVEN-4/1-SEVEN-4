5d80948343470bf77b1913e2582c3034406f24ca
ohhyeon2 <dhgus1235@gmail.com>
2025-03-04 09:53:45 +0900

    feat: prisma schema 작성

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-import { PORT } from './config/index.js'
+import { PORT } from './config/index.js';
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use('', '')
+app.use('', '');
 
-app.listen(PORT || 3000, () => console.log(`server on ${PORT}`))
+app.listen(PORT || 3000, () => console.log(`server on ${PORT}`));
