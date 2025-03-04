4f38667339a2f65cfe38640dc0925ce9bcf7242b
hapyvirus <serenity2011@naver.com>
2025-03-04 19:34:06 +0900

    Creat: 그룹 상세 조회

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-import express from "express";
-import cors from "cors";
-import groupRoute from "./routes/groupRoute.js";
+import express from 'express'
+import cors from 'cors'
+import groupRoute from './routes/groupRoute.js'
 
-import { PORT } from "./config/index.js";
+import { PORT } from './config/index.js'
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-app.use('/groups', groupRoute);
+app.use('/groups', groupRoute)
 
-app.listen(PORT || 3000, () => console.log(`server on ${PORT}`));
+app.listen(PORT || 3000, () => console.log(`server on ${PORT}`))
