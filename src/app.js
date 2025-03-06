4f9747181a3d31176d13265dce65acf43292a5a7
MINSOO SHIN <minso0317@naver.com>
2025-03-07 03:01:38 +0900

    feat: router group, record Router 추가

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
-import express from 'express'
-import cors from 'cors'
+/* eslint-disable prettier/prettier */
+import express from 'express';
+import cors from 'cors';
+import { PORT } from './config/index.js';
+import groupRouter from './routes/member.js';
+import recordRouter from './routes/record.js';
 
-import { PORT } from './config/index.js'
+const app = express();
+app.use(cors());
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+app.use(express.json());
 
-app.use('', '')
+app.use('/groups', groupRouter);
 
-app.listen(PORT || 3000, () => console.log(`server on ${PORT}`))
+app.use('/records', recordRouter);
+
+app.listen(PORT || 3000, () => console.log(`server on ${PORT}`));
