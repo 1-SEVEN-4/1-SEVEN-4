45d6a36c54c8df209815f5ea341c93a826cf2a43
hapyvirus <serenity2011@naver.com>
2025-03-06 17:18:36 +0900

    feat: likeCount Delete 기능 및 에러 처리 코드 추가

--- src/routes/groupRoute.js
+++ src/routes/groupRoute.js
@@ -4,29 +4,55 @@ import express from 'express';
 const prisma = new PrismaClient();
 const groupRoute = express.Router();
 
-groupRoute.route('/:id/likes').post(async (req, res) => {
-  try {
-    const { id } = req.params;
-    const group = await prisma.group.findUnique({
-      where: { id },
-    });
+groupRoute
+  .route('/:id/likes')
+  .post(async (req, res) => {
+    try {
+      const { id } = req.params;
+      const group = await prisma.group.findUnique({
+        where: { id },
+      });
 
-    const updategroup = await prisma.group.update({
-      where: { id },
-      data: { likeCount: (group.likeCount += 1) },
-    });
+      const updategroup = await prisma.group.update({
+        where: { id },
+        data: { likeCount: (group.likeCount += 1) },
+      });
 
-    res.status(201).send(updategroup);
-  } catch (e) {
-    if (
-      e instanceof Prisma.PrismaClientKnownRequestError &&
-      e.code === 'P2023'
-    ) {
-      res.status(404).send({ message: '그룹을 찾을 수 없습니다.' });
-    } else {
-      res.status(500).send({ message: '서버에 문제가 발생했습니다.' });
+      res.status(201).send(updategroup);
+    } catch (e) {
+      if (
+        e instanceof Prisma.PrismaClientKnownRequestError &&
+        e.code === 'P2023'
+      ) {
+        res.status(404).send({ message: '그룹을 찾을 수 없습니다.' });
+      } else {
+        res.status(500).send({ message: '서버에 문제가 발생했습니다.' });
+      }
     }
-  }
-});
+  })
+  .delete(async (req, res) => {
+    try {
+      const { id } = req.params;
+      const group = await prisma.group.findUnique({
+        where: { id },
+      });
+
+      const updategroup = await prisma.group.update({
+        where: { id },
+        data: { likeCount: (group.likeCount -= 1) },
+      });
+
+      res.status(200).send(updategroup);
+    } catch (e) {
+      if (
+        e instanceof Prisma.PrismaClientKnownRequestError &&
+        e.code === 'P2023'
+      ) {
+        res.status(404).send({ message: '그룹을 찾을 수 없습니다.' });
+      } else {
+        res.status(500).send({ message: '서버에 문제가 발생했습니다.' });
+      }
+    }
+  });
 
 export default groupRoute;
