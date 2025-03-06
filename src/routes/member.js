98a3a0cf9bbe1d2ceb81ac575a75d638ce23d15e
MINSOO SHIN <minso0317@naver.com>
2025-03-07 02:17:49 +0900

    refactor: groupId로 member 관리 groupId를 이용하여 member 생성, 삭제 및 조회 하도록 리팩토링

--- src/routes/member.js
+++ src/routes/member.js
@@ -1,24 +1,54 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
-import { validateLoginCondition, validateLoginInfo } from './validateLogin.js';
+import {
+  validateLoginCondition,
+  validateLoginInfo,
+  validateGroupId,
+} from './validateLogin.js';
 
 const prisma = new PrismaClient();
-const memberRouter = express.Router();
+const groupRouter = express.Router();
 
-memberRouter.route('/').post(validateLoginCondition(), async (req, res) => {
-  const member = await prisma.member.create({
-    data: req.body,
+groupRouter
+  .route('/:groupId/members')
+  .post(validateLoginCondition(), validateGroupId(), async (req, res) => {
+    const { groupId } = req.params;
+    const { nickname, password } = req.body;
+
+    const newMember = await prisma.member.create({
+      data: {
+        nickname,
+        password,
+        groupId,
+      },
+    });
+
+    return res.status(201).json(newMember);
+  })
+  .get(validateGroupId(), async (req, res) => {
+    const { groupId } = req.params;
+
+    const members = await prisma.member.findMany({
+      where: {
+        groupId,
+      },
+    });
+
+    return res.status(200).json(members);
+  })
+  .delete(validateGroupId(), validateLoginInfo(), async (req, res) => {
+    const { groupId } = req.params;
+    const { nickname } = req.body;
+
+    const member = await prisma.member.findUnique({
+      where: { groupId, nickname },
+    });
+
+    await prisma.member.delete({
+      where: { id: member.id },
+    });
+
+    return res.status(204).send();
   });
-  res.status(201).send(member);
-});
 
-memberRouter.route('/:id').delete(validateLoginInfo(), async (req, res) => {
-  const { id } = req.params;
-
-  await prisma.member.delete({
-    where: { id },
-  });
-  res.status(204).send();
-});
-
-export default memberRouter;
+export default groupRouter;
