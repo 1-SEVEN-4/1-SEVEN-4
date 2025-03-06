0563a396fd6a34625704bc673b22df6e482218c0
hapyvirus <serenity2011@naver.com>
2025-03-06 17:03:14 +0900

    feat: likeCount post 에러 처리 코드 추가

--- src/routes/groupRoute.js
+++ src/routes/groupRoute.js
@@ -5,17 +5,28 @@ const prisma = new PrismaClient();
 const groupRoute = express.Router();
 
 groupRoute.route('/:id/likes').post(async (req, res) => {
-  const { id } = req.params;
-  const group = await prisma.group.findUnique({
-    where: { id },
-  });
+  try {
+    const { id } = req.params;
+    const group = await prisma.group.findUnique({
+      where: { id },
+    });
 
-  const updategroup = await prisma.group.update({
-    where: { id },
-    data: { likeCount: (group.likeCount += 1) },
-  });
+    const updategroup = await prisma.group.update({
+      where: { id },
+      data: { likeCount: (group.likeCount += 1) },
+    });
 
-  res.status(201).send(updategroup);
+    res.status(201).send(updategroup);
+  } catch (e) {
+    if (
+      e instanceof Prisma.PrismaClientKnownRequestError &&
+      e.code === 'P2023'
+    ) {
+      res.status(404).send({ message: '그룹을 찾을 수 없습니다.' });
+    } else {
+      res.status(500).send({ message: '서버에 문제가 발생했습니다.' });
+    }
+  }
 });
 
 export default groupRoute;
