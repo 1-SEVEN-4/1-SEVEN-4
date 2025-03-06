bedca45e9d0f3c70ee01cccf5b0c5c9d59ec2588
hapyvirus <serenity2011@naver.com>
2025-03-06 15:09:29 +0900

    feat: Group 상세 조회

---
+++ src/routes/groupRoute.js
@@ -0,0 +1,46 @@
+import { Prisma, PrismaClient } from '@prisma/client'
+import express from 'express'
+
+const prisma = new PrismaClient()
+const groupRoute = express.Router()
+
+groupRoute.route('/').post(async (req, res) => {
+  const group = await prisma.group.create({
+    data: req.body,
+  })
+  res.status(201).send(group)
+})
+
+groupRoute.route('/:id').get(async (req, res) => {
+  try {
+    const { id } = req.params
+    const group = await prisma.group.findUnique({
+      where: { id },
+      include: {
+        Members: {
+          select: {
+            id: true,
+            nickName: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+        badge: true,
+        ownerPassword: false,
+      },
+    })
+
+    res.status(200).send(group)
+  } catch (e) {
+    if (
+      e instanceof Prisma.PrismaClientKnownRequestError &&
+      e.code === 'P2023'
+    ) {
+      res.status(404).send({ message: '그룹을 찾을 수 없습니다.' })
+    } else {
+      res.status(500).send({ message: '서버에 문제가 발생했습니다.' })
+    }
+  }
+})
+
+export default groupRoute
