4913c534466714f4ad892bb981c6d1af8d8d6c17
hapyvirus <serenity2011@naver.com>
2025-03-07 12:59:29 +0900

    refactor: route -> controllers 정리

--- src/routes/groupRoute.js
+++ src/routes/groupRoute.js
@@ -1,58 +1,11 @@
 import { Prisma, PrismaClient } from '@prisma/client';
 import express from 'express';
+import { BadLikeCount, goodLikeCount } from '../controllers/groupController.js';
 
 const prisma = new PrismaClient();
 const groupRoute = express.Router();
 
-groupRoute
-  .route('/:id/likes')
-  .post(async (req, res) => {
-    try {
-      const { id } = req.params;
-      const group = await prisma.group.findUnique({
-        where: { id },
-      });
-
-      const updategroup = await prisma.group.update({
-        where: { id },
-        data: { likeCount: (group.likeCount += 1) },
-      });
-
-      res.status(200).send(updategroup);
-    } catch (e) {
-      if (
-        e instanceof Prisma.PrismaClientKnownRequestError &&
-        e.code === 'P2023'
-      ) {
-        res.status(404).send({ message: '그룹을 찾을 수 없습니다.' });
-      } else {
-        res.status(500).send({ message: '서버에 문제가 발생했습니다.' });
-      }
-    }
-  })
-  .delete(async (req, res) => {
-    try {
-      const { id } = req.params;
-      const group = await prisma.group.findUnique({
-        where: { id },
-      });
-
-      const updategroup = await prisma.group.update({
-        where: { id },
-        data: { likeCount: (group.likeCount -= 1) },
-      });
-
-      res.status(200).send(updategroup);
-    } catch (e) {
-      if (
-        e instanceof Prisma.PrismaClientKnownRequestError &&
-        e.code === 'P2023'
-      ) {
-        res.status(404).send({ message: '그룹을 찾을 수 없습니다.' });
-      } else {
-        res.status(500).send({ message: '서버에 문제가 발생했습니다.' });
-      }
-    }
-  });
+groupRoute.post('/:id/likes', goodLikeCount);
+groupRoute.delete('/:id/likes', BadLikeCount);
 
 export default groupRoute;
