a28a4ae5e95b4d9b92492db2e6c690dd8cd6ab18
MINSOO SHIN <minso0317@naver.com>
2025-03-07 14:23:21 +0900

    style: router명칭 변경

--- src/app.js
+++ src/app.js
@@ -2,19 +2,13 @@
 import express from 'express';
 import cors from 'cors';
 import { PORT } from './config/index.js';
-import groupRouter from './routes/member.js';
-import recordRouter from './routes/record.js';
-import groupRouters from './routes/group.js';
+import groupMemberRouters from './routes/member.js';
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
 
-app.use('/groups', groupRouter);
-
-app.use('/records', recordRouter);
-
-app.use('/groups', groupRouters);
+app.use('/groups', groupMemberRouters);
 
 app.listen(PORT || 3000, () => console.log(`server on ${PORT}`));
--- src/routes/group.js
+++ src/routes/group.js
@@ -1,45 +1,9 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
+import { createGroup, getGroup } from '../controllers/groupController.js';
 
 const groupRouters = express.Router();
-const prisma = new PrismaClient();
 
-groupRouters
-  .route('/')
-  .get(async (req, res) => {
-    const { offset = 0, limit = 10, order = 'newest' } = req.query;
-    let orderBy;
-    switch (order) {
-      case 'oldest':
-        orderBy = { createsAt: 'asc' };
-        break;
-      case 'newest':
-      default:
-        orderBy = { createdAt: 'desc' };
-    }
-    const groups = await prisma.group.findMany({
-      orderBy,
-      skip: offset,
-      take: limit,
-      include: {
-        members: {
-          select: {
-            id: true,
-            nickName: true,
-            createdAt: true,
-            updatedAt: true,
-            groupId: true,
-          },
-        },
-      },
-    });
-    res.status(200).send(groups);
-  })
-  .post(async (req, res) => {
-    const group = await prisma.group.create({
-      data: req.body,
-    });
-    res.status(201).send(group);
-  });
+groupRouters.get('/', getGroup);
+groupRouters.post('/', createGroup);
 
 export default groupRouters;
