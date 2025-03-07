0ecb6013b2fe90d6f2768245417b65b9b4133248
hapyvirus <serenity2011@naver.com>
2025-03-07 13:07:55 +0900

    style: groupRoute 미사용 import 삭제

--- src/routes/groupRoute.js
+++ src/routes/groupRoute.js
@@ -1,8 +1,6 @@
-import { Prisma, PrismaClient } from '@prisma/client';
 import express from 'express';
 import { BadLikeCount, goodLikeCount } from '../controllers/groupController.js';
 
-const prisma = new PrismaClient();
 const groupRoute = express.Router();
 
 groupRoute.post('/:id/likes', goodLikeCount);
