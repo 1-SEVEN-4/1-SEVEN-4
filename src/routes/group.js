d93cfce99f4463b703886832b558a0806c4cecef
MINSOO SHIN <minso0317@naver.com>
2025-03-07 14:25:16 +0900

    style: 불필요 파일 삭제

--- src/routes/group.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createGroup, getGroup } from '../controllers/groupController.js';
-
-const groupRouters = express.Router();
-
-groupRouters.get('/', getGroup);
-groupRouters.post('/', createGroup);
-
-export default groupRouters;
