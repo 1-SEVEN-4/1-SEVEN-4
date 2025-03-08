b1b78c953c78d341be4ce1e2dcd0958aecb1b2d7
hapyvirus <serenity2011@naver.com>
2025-03-08 19:50:11 +0900

    feat: Prisma Skima 초기화 및 재설정(03/08ver)

--- prisma/migrations/20250306044703_alter_badge/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Group" DROP CONSTRAINT "Group_badgeId_fkey";
-
--- AlterTable
-ALTER TABLE "Group" ALTER COLUMN "badgeId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Group" ADD CONSTRAINT "Group_badgeId_fkey" FOREIGN KEY ("badgeId") REFERENCES "Badge"("badgeId") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20250308104650_prisma_init_reset/migration.sql
+++ prisma/migrations/20250308104650_prisma_init_reset/migration.sql
@@ -1,3 +1,9 @@
+-- CreateEnum
+CREATE TYPE "BadgeType" AS ENUM ('likeCountbadge', 'recordbadge', 'memberBadges');
+
+-- CreateEnum
+CREATE TYPE "Sports" AS ENUM ('RUNNING', 'CYCLING', 'SWIMMING');
+
 -- CreateTable
 CREATE TABLE "Group" (
     "id" UUID NOT NULL,
@@ -14,9 +20,19 @@ CREATE TABLE "Group" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "memberCount" INTEGER NOT NULL DEFAULT 0,
-    "badgeId" UUID,
 
     CONSTRAINT "Group_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GroupBadge" (
+    "groupBadgeId" UUID NOT NULL,
+    "groupBadgeName" "BadgeType" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "groupId" UUID NOT NULL,
+
+    CONSTRAINT "GroupBadge_pkey" PRIMARY KEY ("groupBadgeId")
 );
 
 -- CreateTable
@@ -43,40 +59,15 @@ CREATE TABLE "Members" (
 );
 
 -- CreateTable
-CREATE TABLE "Badge" (
-    "badgeId" UUID NOT NULL,
-    "member" BOOLEAN NOT NULL DEFAULT false,
-    "record" BOOLEAN NOT NULL DEFAULT false,
-    "likeCount" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "memberId" UUID,
-    "recordId" UUID,
-
-    CONSTRAINT "Badge_pkey" PRIMARY KEY ("badgeId")
-);
-
--- CreateTable
-CREATE TABLE "GroupBadge" (
-    "groupBadgeId" UUID NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "badgeId" UUID NOT NULL,
-    "groupId" UUID NOT NULL,
-
-    CONSTRAINT "GroupBadge_pkey" PRIMARY KEY ("groupBadgeId")
-);
-
--- CreateTable
 CREATE TABLE "Record" (
     "id" UUID NOT NULL,
     "nickName" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "sports" TEXT NOT NULL,
+    "sports" "Sports" NOT NULL,
     "description" TEXT NOT NULL,
-    "time" INTEGER NOT NULL,
+    "time" TEXT NOT NULL,
     "distance" INTEGER NOT NULL,
-    "photo" TEXT NOT NULL,
+    "photo" TEXT[],
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "memberId" UUID NOT NULL,
@@ -85,26 +76,20 @@ CREATE TABLE "Record" (
     CONSTRAINT "Record_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Group_name_key" ON "Group"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Group_ownerNickname_key" ON "Group"("ownerNickname");
+
 -- AddForeignKey
-ALTER TABLE "Group" ADD CONSTRAINT "Group_badgeId_fkey" FOREIGN KEY ("badgeId") REFERENCES "Badge"("badgeId") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "GroupBadge" ADD CONSTRAINT "GroupBadge_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "GroupTags" ADD CONSTRAINT "GroupTags_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Members" ADD CONSTRAINT "Members_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Badge" ADD CONSTRAINT "Badge_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "Members"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Badge" ADD CONSTRAINT "Badge_recordId_fkey" FOREIGN KEY ("recordId") REFERENCES "Record"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "GroupBadge" ADD CONSTRAINT "GroupBadge_badgeId_fkey" FOREIGN KEY ("badgeId") REFERENCES "Badge"("badgeId") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "GroupBadge" ADD CONSTRAINT "GroupBadge_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Members" ADD CONSTRAINT "Members_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Record" ADD CONSTRAINT "Record_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "Members"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
