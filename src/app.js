8c2202fb3e536e0db0f945ddef39a1388c864756
MINSOO SHIN <minso0317@naver.com>
2025-03-10 17:41:25 +0900

    memberRoutes 파일명 수정

--- src/app.js
+++ src/app.js
@@ -2,10 +2,7 @@
 import express from 'express';
 import cors from 'cors';
 import { PORT } from './config/index.js';
-import groupMemberRouters from './routes/member.js';
-
-import groupRouters from './routes/group.js';
-import recordListRoutes from './routes/recordListRoutes.js';
+import groupMemberRouters from './routes/memberRoutes.js';
 
 import {
   defaultNotFoundHandler,
@@ -18,9 +15,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/groups', groupMemberRouters);
-app.use('/groups', groupRouters);
-
-app.use('/groups', recordListRoutes);
 
 app.use(defaultNotFoundHandler);
 app.use(globalErrorHandler);
