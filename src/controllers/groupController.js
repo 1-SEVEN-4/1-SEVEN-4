0ddf7a05d7848dfb2d9697dcfff84251106602c9
ohhyeon2 <dhgus1235@gmail.com>
2025-03-11 11:43:04 +0900

    fix: groupController 입출력 내용 수정

--- src/controllers/groupController.js
+++ src/controllers/groupController.js
@@ -1,17 +1,18 @@
-import { Prisma } from '@prisma/client';
 import { validate } from 'superstruct';
 import prisma from '../config/prisma.js';
-import { CreateGroupSchema } from '../util/superstruct.js';
+import { CreateGroupSchema, validationError } from '../util/superstruct.js';
 import { catchHandler } from '../lib/catchHandler.js';
 
 export const createGroup = catchHandler(async (req, res) => {
-  console.log(req.body);
-  const [error, validateData] = validate(req.body, CreateGroupSchema);
-  if (error) return res.status(400).send({ message: error.message });
+  const [error] = validate(req.body, CreateGroupSchema);
+  if (error) {
+    const message = validationError(error);
+    return res.status(400).send({ message });
+  }
+  const { ownerNickname } = req.body;
 
-  console.log(req.body.ownerNickName);
   const existNickName = await prisma.members.findFirst({
-    where: { nickName: validateData.ownerNickName },
+    where: { nickName: ownerNickname },
   });
 
   if (existNickName) {
@@ -20,63 +21,47 @@ export const createGroup = catchHandler(async (req, res) => {
 
   const newGroup = await prisma.group.create({
     data: {
-      name: validateData.name,
-      ownerNickName: validateData.ownerNickName,
-      ownerPassword: validateData.ownerPassword,
-      description: validateData.description,
-      photo: validateData.photo,
-      tags: validateData.tags,
-      goalRep: validateData.goalRep,
-      discordURL: validateData.discordURL,
-      invitationURL: validateData.invitationURL,
+      name: req.body.name,
+      ownerNickName: ownerNickname,
+      ownerPassword: req.body.ownerPassword,
+      description: req.body.description,
+      photo: req.body.photo,
+      tags: req.body.tags,
+      goalRep: req.body.goalRep,
+      discordURL: req.body.discordURL,
+      invitationURL: req.body.invitationURL,
       likeCount: 0,
       memberCount: 1,
       Members: {
         create: {
-          nickName: validateData.ownerNickName,
-          password: validateData.ownerPassword,
+          nickName: ownerNickname,
+          password: req.body.ownerPassword,
         },
       },
     },
+    include: { Members: true, badge: true },
   });
 
-  return res.status(201).send(newGroup);
-});
-
-export const updateGroup = catchHandler(async (req, res) => {
-  const { groupId } = req.params;
-  const { ownerNickName, ownerPassword, updateData } = req.body;
-
-  const group = await prisma.group.findUnique({ where: { id: groupId } });
-  if (!group) return res.status(404).send({ message: '그룹을 찾을 수 없습니다.' });
-
-  if (ownerNickName !== group.ownerNickName || ownerPassword !== group.ownerPassword) {
-    return res.status(401).send({ message: '닉네임 혹은 비밀번호를 확인해주세요.' });
-  }
-
-  if (typeof updateData.goalCount !== 'number') {
-    return res.status(400).send({ message: '목표 횟수는 숫자여야 합니다.' });
-  }
-
-  const updatedGroup = await prisma.group.update({
-    where: { id: groupId },
-    data: updateData,
-  });
-
-  return res.status(200).send(updatedGroup);
-});
-
-export const deleteGroup = catchHandler(async (req, res) => {
-  const { groupId } = req.params;
-  const { ownerPassword } = req.body;
-
-  const group = await prisma.group.findUnique({ where: { id: groupId } });
-  if (!group) return res.status(404).send({ message: '그룹을 찾을 수 없습니다.' });
-
-  if (ownerPassword !== group.ownerPassword) {
-    return res.status(401).send({ message: '닉네임 혹은 비밀번호를 확인해주세요' });
-  }
-
-  await prisma.group.delete({ where: { id: groupId } });
-  return res.status(200).send({ message: '그룹이 삭제되었습니다.' });
+  const result = {
+    id: newGroup.id,
+    name: newGroup.name,
+    description: newGroup.description,
+    photo: newGroup.photo,
+    goalRep: newGroup.goalRep,
+    discordURL: newGroup.discordURL,
+    invitationURL: newGroup.invitationURL,
+    likeCount: newGroup.likeCount,
+    tags: newGroup.tags,
+    ownerNickName: newGroup.ownerNickName,
+    members: newGroup.Members.map(member => ({
+      id: member.id,
+      nickname: member.nickName,
+      createdAt: new Date(member.createdAt).getTime(),
+      updatedAt: new Date(member.updatedAt).getTime(),
+    })),
+    createdAt: new Date(newGroup.createdAt).getTime(),
+    updatedAt: new Date(newGroup.updatedAt).getTime(),
+    badges: newGroup.badge ? [newGroup.badge.badgeId] : [],
+  };
+  return res.status(201).send(result);
 });
