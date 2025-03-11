c0296106147ed2278f8858263eb2acd8b4b2538e
ohhyeon2 <dhgus1235@gmail.com>
2025-03-11 11:18:13 +0900

    groupRoute.js의 사용하지 않는 부분 제거

--- src/routes/groupRoute.js
+++ src/routes/groupRoute.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import { BadLikeCount, goodLikeCount } from '../controllers/groupController.js';
 import { createGroup, updateGroup, deleteGroup } from '../controllers/groupController.js';
 
 const groupRoute = express.Router();
@@ -7,7 +6,5 @@ const groupRoute = express.Router();
 groupRoute.post('/', createGroup);
 groupRoute.put('/:groupId', updateGroup);
 groupRoute.delete('/:groupId', deleteGroup);
-groupRoute.post('/:id/likes', goodLikeCount);
-groupRoute.delete('/:id/likes', BadLikeCount);
 
 export default groupRoute;
