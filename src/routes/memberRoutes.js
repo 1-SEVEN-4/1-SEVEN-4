0d7e3343858ff38d935f74faf5f0b7350aa6c4cd
MINSOO SHIN <minso0317@naver.com>
2025-03-14 00:52:22 +0900

    routes에 validator 추가

--- src/routes/memberRoutes.js
+++ src/routes/memberRoutes.js
@@ -1,9 +1,5 @@
 import express from 'express';
-import {
-  createMember,
-  deleteMember,
-  getMember,
-} from '../controllers/memberController.js';
+import { createMember, deleteMember, getMember } from '../controllers/memberController.js';
 
 import validateGroupId from '../lib/validator/validateGroupId.js';
 import validateLoginCondition from '../lib/validator/validateLoginCondition.js';
@@ -11,18 +7,8 @@ import validateLoginInfo from '../lib/validator/validateLoginInfo.js';
 
 const groupMemberRouters = express.Router();
 
-groupMemberRouters.post(
-  '/:groupId/members',
-  validateLoginCondition,
-  validateGroupId,
-  createMember,
-);
+groupMemberRouters.post('/:groupId/members', validateLoginCondition, validateGroupId, createMember);
 groupMemberRouters.get('/:groupId/members', validateGroupId, getMember);
-groupMemberRouters.delete(
-  '/:groupId/members',
-  validateGroupId,
-  validateLoginInfo,
-  deleteMember,
-);
+groupMemberRouters.delete('/:groupId/members', validateGroupId, validateLoginInfo, deleteMember);
 
 export default groupMemberRouters;
--- src/routes/recordListRoutes.js
+++ src/routes/recordListRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import getRecordList from '../controllers/recordListController.js';
+import validateGroupId from '../lib/validator/validateGroupId.js';
+import validateLoginCondition from '../lib/validator/validateLoginCondition.js';
+import validateLoginInfo from '../lib/validator/validateLoginInfo.js';
 
 const recordListRoutes = express.Router();
 
-recordListRoutes.get('/:groupId/records', getRecordList);
+recordListRoutes.get('/:groupId/records', validateGroupId, getRecordList);
 
 export default recordListRoutes;
