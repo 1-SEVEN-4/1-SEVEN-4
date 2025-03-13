94531eb0280e6a9e8377ce2f9311d251e391a2dc
hapyvirus <serenity2011@naver.com>
2025-03-13 17:53:44 +0900

    feat: 웹훅 알림, 이미지 경로

--- src/controllers/recordController.js
+++ src/controllers/recordController.js
@@ -12,8 +12,9 @@ export const createRecord = catchHandler(async (req, res) => {
     where: { groupId, nickName },
   });
 
+  console.log(groupId, nickName);
   if (!member) {
-    return res.status(400).send({ message: '닉네임 또는 비밀번호를 확인해주세요.' });
+    return res.status(400).send({ message: '닉네임 확인해주세요.' });
   }
 
   const group = await prisma.group.findUnique({
@@ -21,7 +22,7 @@ export const createRecord = catchHandler(async (req, res) => {
   });
 
   if (member.password !== password) {
-    return res.status(400).send({ message: '닉네임 또는 비밀번호를 확인해주세요.' });
+    return res.status(400).send({ message: '비밀번호를 확인해주세요.' });
   }
 
   const timerData = stopTimer();
@@ -43,7 +44,7 @@ export const createRecord = catchHandler(async (req, res) => {
     id: record.id,
     sports,
     description,
-    time,
+    time: record.time,
     distance,
     photo: record.photo.map(photoPath => {
       return `${PORT}/${photoPath}`;
@@ -58,7 +59,7 @@ export const createRecord = catchHandler(async (req, res) => {
     createdAt: record.createdAt,
     updatedAt: record.updatedAt,
   };
-  discordNotice(group.name, nickName);
+  discordNotice(group.invitationURL, group.name, nickName);
   res.status(201).send(response);
 });
 
@@ -106,8 +107,8 @@ export async function getRecordDetail(req, res) {
       distance: record.distance,
       photo: record.photo ? record.photo.split(',') : [],
       members: {
-        id: record.member.id,
-        nickname: record.member.nickName,
+        id: record.members.id,
+        nickname: record.members.nickName,
       },
     });
   } catch (error) {
--- src/util/noticeUtil.js
+++ src/util/noticeUtil.js
@@ -1,18 +1,7 @@
 import { Webhook, MessageBuilder } from 'discord-webhook-node';
-import dotenv from 'dotenv';
-import { fileURLToPath } from 'url';
-import path from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
-
-console.log(process.env.HOOK_URL);
-
-const hook = new Webhook(process.env.HOOK_URL);
-
-export default async function discordNotice(name, nickName) {
+export default async function discordNotice(url, name, nickName) {
+  const hook = new Webhook(url);
   const embed = new MessageBuilder()
     .setTitle(`${name}`)
     .setColor('#00ff00')
