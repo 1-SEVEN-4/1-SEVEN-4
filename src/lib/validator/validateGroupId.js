eb95bd0399c5fa701e2a46c35d0a2ea1b626e9ff
MINSOO SHIN <minso0317@naver.com>
2025-03-14 00:48:55 +0900

    validator에 catchHanlder추가

--- src/lib/validator/validateGroupId.js
+++ src/lib/validator/validateGroupId.js
@@ -1,21 +1,17 @@
 import prisma from '../../config/prisma.js';
+import { catchHandler } from '../catchHandler.js';
 
-export default async function validateGroupId(req, res, next) {
-  try {
-    const { groupId } = req.params;
-    const group = await prisma.group.findUnique({
-      where: { id: groupId },
-    });
+const validateGroupId = catchHandler(async (req, res, next) => {
+  const { groupId } = req.params;
+  const group = await prisma.group.findUnique({
+    where: { id: groupId },
+  });
 
-    if (!group) {
-      return res.status(400).send({ message: '그룹이 존재하지 않습니다.' });
-    }
-
-    return next();
-  } catch (e) {
-    console.log('Error occured', e);
-    return res.status(500).send({ message: '서버에 문제가 발생했습니다.' });
-  } finally {
-    console.log('Finished');
+  if (!group) {
+    return res.status(400).send({ message: '그룹이 존재하지 않습니다.' });
   }
-}
+
+  return next();
+});
+
+export default validateGroupId;
