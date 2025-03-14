932dbd40a0f2a7dfd2d09c5e5c7dd0e37a11bbed
MINSOO SHIN <minso0317@naver.com>
2025-03-14 15:49:14 +0900

    미사용 import 제거

--- src/routes/recordListRoutes.js
+++ src/routes/recordListRoutes.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import getRecordList from '../controllers/recordListController.js';
 import validateGroupId from '../lib/validator/validateGroupId.js';
-import validateLoginCondition from '../lib/validator/validateLoginCondition.js';
-import validateLoginInfo from '../lib/validator/validateLoginInfo.js';
 
 const recordListRoutes = express.Router();
 
