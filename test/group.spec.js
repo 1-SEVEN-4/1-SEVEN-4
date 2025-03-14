ed40bc2a584316375d7879928302627ed2c06f4c
ohhyeon2 <dhgus1235@gmail.com>
2025-03-14 09:30:01 +0900

    test set

--- test/group.spec.js
+++ test/group.spec.js
@@ -1,4 +1,10 @@
-import { groupService } from '../src/controller/groupController';
-import prisma from '../src/config/prisma';
+import { groupService } from '../src/controllers/groupController.js';
+import prisma from '../src/config/prisma.js';
 
-jest.mock('/src/config/prisma.js');
+jest.mock('../src/config/prisma.js');
+
+describe('그룹 API tests', () => {
+  afterEach(() => {
+    jest.clearAllMocks;
+  });
+});
