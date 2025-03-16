b8ae4eefe69c371d59f3f39eaae0c693ae58cd08
MINSOO SHIN <minso0317@naver.com>
2025-03-17 02:43:32 +0900

    feat: validator 미드웨어 적용

--- src/routes/rankingRoute.js
+++ src/routes/rankingRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import rankingController from '../controllers/rankingController.js';
+import validateGroupId from '../lib/validator/validateGroupId.js';
 
 const rankingsRoute = express.Router();
 
-rankingsRoute.get('/:groupId/ranking/weekly', rankingController.getWeeklyRanking);
-rankingsRoute.get('/:groupId/ranking/monthly', rankingController.getMonthlyRanking);
+rankingsRoute.get('/:groupId/ranking/weekly', validateGroupId, rankingController.getWeeklyRanking);
+rankingsRoute.get('/:groupId/ranking/monthly', validateGroupId, rankingController.getMonthlyRanking);
 export default rankingsRoute;
--- src/routes/recordRoutes.js
+++ src/routes/recordRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { createRecord } from '../controllers/recordController.js';
 import { getRecordDetail } from '../controllers/recordController.js';
+import validateGroupId from '../lib/validator/validateGroupId.js';
+import validateLoginInfo from '../lib/validator/validateLoginInfo.js';
 import { startTimer } from '../util/timeUtil.js';
 
 const recordRoute = express.Router();
 
-recordRoute.get('/:groupId/records/:recordId', getRecordDetail);
-recordRoute.post('/:groupId/records', createRecord);
+recordRoute.get('/:groupId/records/:recordId', validateGroupId, getRecordDetail);
+recordRoute.post('/:groupId/records', validateGroupId, validateLoginInfo, createRecord);
 recordRoute.post('/start', startTimer);
 
 export default recordRoute;
